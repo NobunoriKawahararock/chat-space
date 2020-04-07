334b7ecd543288bc9d5a5831675dffaee2d6f823
NobunoriKawahararock <rokon_cyubu@yahoo.co.jp>
2020-04-07 20:06:00 +0900

    master避け用非同期通信

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,7 @@
 class Message < ApplicationRecord
+  validates :text, presence: true, unless: :image?
   belongs_to :group
   belongs_to :user
-
-  validates :text, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
 end
